f9cad1e228a91c14fb602e7b18ace4289ebc0aec
rayna williams <Raynawilliams123@gmail.com>
2019-12-12 17:05:22 -0700

    origin.master

---
+++ script.js
@@ -0,0 +1,24 @@
+var password =
+var generateBtn = document.getElementById("#generate");
+var upperCaseLetter = document.querySelector("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+var lowerCaseLetter = document.querySelector("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+var numbers = document.querySelector(1,2,3,4,5,6,7,8,9);
+var potentialCharacters = document.querySelector("!","@","&","*","?","^","/","~","(",")","-","#","%");
+// insert password 
+var password = generatePassword(); [
+    console.log(generatePassword); {
+        
+    }
+]
+
+
+
+// password should be 8-15 characters long
+
+
+
+// click generate password
+
+
+
+// copy and save password in clipboard 
